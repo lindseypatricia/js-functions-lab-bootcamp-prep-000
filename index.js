9fcc6a46a7d1a4bbc1a90fc41341c2d081c92a60
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-19 04:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return `Happy holidays!`
+}
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday) {
+  return `It's ${days} until ${holiday}!`
+}
+happyHolidays()
+happyHolidaysTo("Festivus", "George")
+holidayCountdown("12", "Festivus")
